7ba62a8ca7ecb7e225b1cbc72e28f8df99dd27d8
Bl3r3 <carrasquel18@gmail.com>
2021-05-30 15:49:30 -0400

    first commit

--- src/main.py
+++ src/main.py
@@ -3,13 +3,13 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from flask import Flask, request, jsonify, url_for
+from flask.globals import g
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, Contact, Group
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -20,24 +20,107 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/contact/all', methods=['GET'])
+def get_contact():
+    contacts = Contact.contact_all();
+    contact_list_dict = list(map(lambda elem: elem.serialize(), contacts))
+    return jsonify(contact_list_dict), 200 
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route("/contact", methods=['POST'])
+def create_contact():
+    """Recibe un nuevo contacto"""
+    #request in json
+    request_body = request.json
+
+    new_contact = Contact.create(
+        full_name = request_body["full_name"],
+        email = request_body["email"],
+        address = request_body["address"],
+        phone = request_body["phone"],
+        memberships = request_body["memberships"]
+    )
+
+    #Save and commit 
+    new_contact.save()
+    #Return a response
+    return jsonify(new_contact.serialize()), 200
+
+
+
+@app.route('/contact/<int:id>', methods=['GET', 'DELETE', 'PUT'])
+def contact_by_id(id):
+    if request.method == 'DELETE':
+        contact = Contact.get_contact_by_id(id);
+        contact.delete_contact_by_id(contact);
+        response_body = {
+            "msg": f"user {id} deleted"
+        }
+        return jsonify(response_body), 200
+
+    if request.method == 'PUT':
+       contact = Contact.query.filter_by(id=id).first();
+       request_body = request.json
+       contact.full_name =  request_body["full_name"]
+       contact.email = request_body["email"]
+       contact.address = request_body["address"]
+       contact.phone = request_body["phone"]
+
+       contact.save()
+
+       return jsonify(contact.serialize()), 200
+    
+        
+    contact = Contact.get_contact_by_id(id)
+    return jsonify(contact.serialize()), 200
+    
+
+
+@app.route('/group/all', methods=['GET'])
+def get_all():
+    groups = Group.get_all_groups();
+    gruop_to_dict = list(map(lambda elem: elem.serialize(), groups))
+    return jsonify(gruop_to_dict), 200
+
+
+@app.route('/group', methods=['POST'])
+def create_a_gruop():
+    request_body = request.json
+    new_group = Group.create(
+        name = request_body["name"]
+    )
+     #Save and commit 
+    new_group.save()
+    #Return a response
+    return jsonify(new_group.serialize()), 200
+
+
+
+@app.route('/group/<int:id>', methods=['GET', 'DELETE', 'PUT'])
+def gruop_by_id(id):
+    if request.method == 'DELETE':
+        group = Group.get_group_by_id(id)
+        group.delete_group_by_id(group)
+        response_body = {
+            "msg": f"group {id} deleted"
+        }
+        return jsonify(response_body), 200
+
+    if request.method == 'PUT':
+       group = Group.query.filter_by(id=id).first();
+       request_body = request.json
+       group.name =  request_body["name"]
+
+       group.save()
+
+       return jsonify(group.serialize()), 200
+
+
+    group = Group.get_group_by_id(id)
+    return jsonify(group.serialize()), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,93 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Contact(db.Model):
+    __tablename__ = "contact"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    full_name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(50))
+    memberships = db.relationship('Membership', backref="contact")
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    @classmethod
+    def create(cls, **kwargs):
+        """Create and return a new instance"""
+        return cls(**kwargs)
+    
+    @classmethod
+    def contact_all(cls):
+        """ Return all contacts as a json """
+        return cls.query.all()
+
+    @classmethod
+    def get_contact_by_id(cls, id):
+        """Get a contact for your id"""
+        return cls.query.get(id)
+    
+    @classmethod
+    def delete_contact_by_id(cls,id):
+        """"Delete a contact by id"""
+        db.session.delete(id) 
+        db.session.commit();
+
+    def save(self):
+        """Save and commit a new contact"""
+        db.session.add(self)
+        db.session.commit()
 
     def serialize(self):
         return {
             "id": self.id,
+            "full_name": self.full_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "address": self.address,
+            "phone": self.phone,
+            "memberships": [m.group.serialize() for m in self.memberships],
         }
+
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    memberships = db.relationship('Membership', backref="group")
+
+    @classmethod
+    def get_all_groups(cls):
+        return cls.query.all();
+
+    @classmethod
+    def create(cls, **kwargs):
+        return cls(**kwargs)
+
+    @classmethod
+    def get_group_by_id(cls, id):
+        """Get a contact for your id"""
+        return cls.query.get(id)
+
+    @classmethod
+    def delete_group_by_id(cls,id):
+        """"Delete a contact by id"""
+        db.session.delete(id) 
+        db.session.commit();
+    
+    def save(self):
+        """Save and commit a new contact"""
+        db.session.add(self)
+        db.session.commit()  
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "memberships": [m.contact.serialize() for m in self.memberships],
+        }
+
+
+
+class Membership(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    contact_id = db.Column(db.Integer, db.ForeignKey('contact.id'))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+
